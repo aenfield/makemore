c3a7c6e24b1b4f9206d60bb0aacbb859b73d7aa3
Andrew Enfield <aenfield@gmail.com>
2025-03-22 21:06:38 -0700

    Added residual pathway and tweaks to help train better now that the network's getting pretty deep... val loss down to 2.07, and train loss just under 2 (the difference/divergence pointing toward some overfitting).

--- reproducegpt/transformer.py
+++ reproducegpt/transformer.py
@@ -97,9 +97,12 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd) # to rejoin residual pathway?
 
     def forward(self, x):
-        return torch.cat([h(x) for h in self.heads], dim=-1)
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
 
 class FeedForward(nn.Module):
     """ Simple linear layer followed by a non-linearity. """
@@ -107,12 +110,32 @@ class FeedForward(nn.Module):
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd), # to rejoin, like the proj in MultiHeadAttention
         )
 
     def forward(self, x):
         return self.net(x)
+
+class Block(nn.Module):
+    """ Transformer block: communication with self-attention followed by computation with feed-forward. """
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+
+    def forward(self, x):
+        # x = self.sa(x)
+        # x = self.ffwd(X)
+        # do w/ residual connections to help w/ optimization/training since the network's becoming pretty deep
+        x = x + self.sa(x)
+        x = x + self.ffwd(x)
+        return x
+
 
 # very simple transformer model
 class TransformerLanguageModel(nn.Module):
@@ -122,8 +145,11 @@ class TransformerLanguageModel(nn.Module):
         # read the logits for the next token directly from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd) # each position in the context/block gets its own embedding vector
-        self.sa_heads = MultiHeadAttention(4, n_embd//4) # so, four 8D self-attention heads
-        self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+        )
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -134,8 +160,7 @@ class TransformerLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C) # C here is n_embd
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B, T, C) # combine emb for tokens and position, due to broadcasting (T,C) across the batches
-        x = self.sa_heads(x) # apply to all the self-attention heads (B, T, C)
-        x = self.ffwd(x) # (B, T, C)
+        x = self.blocks(x) # (B, T, C)
         logits = self.lm_head(x) # (B,T,C) # C is here vocab_size
 
         if targets is None:
